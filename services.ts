604a41df0b213f0b655840bb2afe35544c631bf3
Max Salvado <43208073+maxSalvado@users.noreply.github.com>
2025-08-15 19:47:19 -0300

    Update services.ts

--- services.ts
+++ services.ts
@@ -104,5 +104,8 @@ export class GlobalLoadingComponent {
 
 ---------------
   
+import { HttpContextToken } from '@angular/common/http';
+
+export const SKIP_LOADING = new HttpContextToken<boolean>(() => false);
 
   
